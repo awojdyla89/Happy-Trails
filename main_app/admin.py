4041c93df28bf4222c9d5d98aa99cb82eee13949
Allie Delano <alliedelano@Allies-MacBook-Pro.local>
2021-07-12 16:44:18 -0700

    adds crud operations for trail and ability to add and view comments

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Trail
+from .models import Trail, Comment
 
 # Register your models here.
 admin.site.register(Trail)
+admin.site.register(Comment)
