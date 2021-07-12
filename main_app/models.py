1d677d398f1574b6452213f4cb129bc602c603a0
Adam Wojdyla <awojdyla@gmail.com>
2021-07-12 14:50:36 -0600

    adding create/edit trail route

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Trail(models.Model):
@@ -8,6 +9,17 @@ class Trail(models.Model):
     state = models.CharField(max_length=100)
     trail_length = models.DecimalField(max_digits=4, decimal_places=2, default= 00.00)
  
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    date = models.DateField('Comment Date')
+    description = models.TextField(max_length=250)
+
+    trail = models.ForeignKey(Trail, on_delete=models.CASCADE)
 
     
+
+# class Photo(models.Model):
+#     url = models.CharField(max_length=200)
+
+#     trail = models.ForeignKey(Trail, on_delete=models.CASCADE)
