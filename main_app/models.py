18c72dfed9ef9e157180f04766942e564ff847d1
Shannon Hensley <shannonshensley96@gmail.com>
2021-07-13 11:09:53 -0500

    added styling

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -8,6 +9,9 @@ class Trail(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     trail_length = models.DecimalField(max_digits=4, decimal_places=2, default= 00.00)
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pk': self.id})
  
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -16,8 +20,13 @@ class Comment(models.Model):
     description = models.TextField(max_length=250)
 
     trail = models.ForeignKey(Trail, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"comment from {self.trail} on {self.date}"
     
+    class Meta:
+        ordering = ['-date']
 
 # class Photo(models.Model):
 #     url = models.CharField(max_length=200)
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,15 @@
-from .models import Trail
-
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
+
+from .models import Trail, Comment
+from .forms import CommentForm
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -16,10 +22,55 @@ def about(request):
 class TrailList(ListView):
     model = Trail
 
-class TrailDetail(DetailView):
-    model = Trail
+def trails_detail(request, trail_id):
+    trail = Trail.objects.get(id=trail_id)
+    comment_form = CommentForm()
+    return render(request, 'trails/detail.html', {
+        'trail': trail, 'comment_form': comment_form
+    })
+
+# class TrailDetail(DetailView):
+#     model = Trail
+#     comment_form = CommentForm()
 
 class TrailCreate(CreateView):
     model = Trail
     fields = ['name', 'address', 'city', 'state', 'trail_length']
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    success_url = '/trails/'
 
+
+class TrailUpdate(UpdateView):
+    model = Trail
+    fields = ['name', 'address', 'city', 'state', 'trail_length']
+
+class TrailDelete(DeleteView):
+    model = Trail
+    success_url = '/trails/'
+
+
+
+def add_comment(request, trail_id):
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        new_comment = form.save(commit=False)
+        new_comment.trail_id = trail_id
+        form.instance.user = request.user
+        new_comment.save()
+    return redirect('detail', trail_id=trail_id)
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - please try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
