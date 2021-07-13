67fd9fd724464a28f9ed343656827995dfca9d4d
Adam Wojdyla <awojdyla@gmail.com>
2021-07-13 16:47:15 -0600

    creating CRUD functions for comments and amenities

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Trail, Comment
+from .models import Trail, Comment, Amenity
 
 # Register your models here.
 admin.site.register(Trail)
 admin.site.register(Comment)
+admin.site.register(Amenity)
